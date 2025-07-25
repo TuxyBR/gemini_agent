50cd2d71798319c492757a52c8c213491aab9779
TuxyBR <rafinhalm.lm@gmail.com>
2025-07-25 17:58:30 -0400

    test files

--- tests.py
+++ tests.py
@@ -1,24 +1,28 @@
+import subprocess
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 def tests():
-  result = get_files_info("calculator", ".")
-  print("Result for current directory:")
+  result = run_python_file("calculator", "main.py")
   print(result)
-  print("")
 
-  result = get_files_info("calculator", "pkg")
-  print("Result for 'pkg' directory:")
+  result = run_python_file("calculator", "main.py", ["3 + 5"])
   print(result)
-  print("")
 
-  result = get_files_info("calculator", "/bin")
-  print("Result for '/bin' directory:")
+  result = run_python_file("calculator", "tests.py")
   print(result)
-  print("")
-  
-  result = get_files_info("calculator", "../")
-  print("Result for '../' directory:")
+
+  result = run_python_file("calculator", "../main.py")
   print(result)
-  
+
+  result = run_python_file("calculator", "nonexistent.py")
+  print(
+    f'STDOUT:\n{result.stdout}' if len(result.stdout) else f'No output produced.' + 
+    f'\nSTDERR:{result.stderr}' if len(result.stderr) else '' +
+    f'\nProcess exited with code {result.returncode}' if result.returncode else ''
+    ) if isinstance(result, subprocess.CompletedProcess) else print (result)
+
 if __name__ == "__main__":
   tests()
