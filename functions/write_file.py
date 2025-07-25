a0fab2705fd22f567f3af758425ba41ffe2f7aa1
TuxyBR <rafinhalm.lm@gmail.com>
2025-07-25 14:58:52 -0400

    small code refactor and variable change

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,13 +2,14 @@ import os
 
 def write_file(working_directory, file_path, content):
   full_path = os.path.abspath(os.path.join(working_directory, file_path))
-  if full_path.startswith(os.path.abspath(working_directory)):
+  abs_working_directory = os.path.abspath(working_directory)
+  if full_path.startswith(abs_working_directory):
     try:
-      if not os.path.exists(working_directory):
-        os.makedirs(working_directory)
+      if not os.path.exists(abs_working_directory):
+        os.makedirs(abs_working_directory)
       with open(full_path, "w") as f:
         f.write(content)
       return f'Successfully wrote to "{file_path}" ({len(content)} characters written)' 
     except Exception as e:
-      return f'Error reading file "{file_path}": {e}'
-  return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+      return f'Error reading file "{full_path}": {e}'
+  return f'Error: Cannot read "{full_path}" as it is outside the permitted working directory ({abs_working_directory})'
