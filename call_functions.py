0d5b732e848b959dd3c0a31c7f3ce4f12cd7fb8f
TuxyBR <rafinhalm.lm@gmail.com>
2025-07-30 15:19:30 -0400

    add a function calling functions

--- call_functions.py
+++ call_functions.py
@@ -1,4 +1,5 @@
-from google.genai import types # type: ignore
+from tabnanny import verbose
+from google.genai import types  # type: ignore
 
 from schemas import *
 
@@ -10,3 +11,32 @@ available_functions = types.Tool(
     schema_write_file,
   ]
 )
+
+
+def call_function(function_call_part, verbose=False):
+  function_name = function_call_part.name
+  if verbose:
+    print(f"Calling function: {function_name}({function_call_part.args})")
+  else:
+    print(f" - Calling function: {function_name}")
+  try:
+    function_result = function_name('./calculator', **function_call_part.args)
+    return types.Content(
+      role="tool",
+      parts=[
+        types.Part.from_function_response(
+          name=function_name,
+          response={"result": function_result},
+        )
+      ],
+    )
+  except Exception as e:
+    return types.Content(
+      role="tool",
+      parts=[
+        types.Part.from_function_response(
+          name=function_name,
+          response={"error": f"Unknown function: {function_name}"},
+        )
+      ],
+    )
